0bf380cc6d23dcb575373f9dd039e76929aca801
abbaspour63 <abbaspourab@gmail.com>
2026-05-07 12:48:36 +0330

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-
 class Projectstate {
     private listeners: any[] = [];
     private projects: any[] = [];
+    //singleton 
     private static instance: Projectstate;
     private constructor() {
     
@@ -13,9 +13,11 @@ class Projectstate {
         this.instance = new Projectstate()
         return this.instance;
     }
+
     addlistener(listenerFn: Function) {
         this.listeners.push(listenerFn)
     }
+
     addproject(title: string, description: string, numofPeople: number) {
         const newProject = {
             id: Math.random().toString(),
@@ -63,7 +65,7 @@ function validate(validatbleInput: validatable) {
     return isvalid;
 
 }
-
+//autobinder 
 function autobinder(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const myDescriptor: PropertyDescriptor = {
@@ -104,7 +106,6 @@ class Projectlist {
             const listItem=document.createElement("li");
             listItem.textContent=prjItem.title;
             listEl.appendChild(listItem)
-
         }
     }
 
@@ -126,15 +127,16 @@ class ProjectInfo {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
-
+    
     constructor() {
         this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
         this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-        const importednode = document.importNode(this.templateElement.content, true)
+        const importednode = document.importNode(this.templateElement.content, true);
         this.element = importednode.firstElementChild as HTMLFormElement;
 
         this.element.id = "user-input";
+
         this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
@@ -183,13 +185,13 @@ class ProjectInfo {
         this.titleInputElement.value = "";
         this.descriptionInputElement.value = "";
         this.peopleInputElement.value = "";
-    this
+    
     }
 
     @autobinder
     private submithandeler(event: Event) {
         event.preventDefault();
-        const userInput = this.getUserInput()
+        const userInput = this.getUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
             projectstate.addproject(title, desc, people)
@@ -200,17 +202,13 @@ class ProjectInfo {
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submithandeler)
+        this.element.addEventListener('submit',this.submithandeler);    
     }
     private attach() {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element)
+        this.hostElement.insertAdjacentElement("afterbegin",this.element)
     }
 }
+
 const prjInput = new ProjectInfo();
 const activePrj = new Projectlist('active')
 const finished = new Projectlist('finished')
-
-
-
-
-
